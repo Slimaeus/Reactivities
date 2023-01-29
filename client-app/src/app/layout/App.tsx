a6c26576d955f81402adfdabbc89abf4beb5cf2d
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-01-29 18:32:36 +0700

    Add style for Nav Bar

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import axios from 'axios';
-import { Header, List } from 'semantic-ui-react';
+import { Container, List } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 
@@ -15,8 +15,9 @@ function App() {
   }, [])
 
   return (
-    <div>
+    <>
       <NavBar />
+      <Container style={{marginTop: '7em'}}>
         <List>
           {activities.map((activity) => (
             <List.Item key={activity.id}>
@@ -24,7 +25,8 @@ function App() {
             </List.Item>
           ))}
         </List>
-    </div>
+      </Container>
+    </>
   );
 }
 
--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -2,10 +2,10 @@ import { Button, Container, Menu } from "semantic-ui-react";
 
 export default function NavBar() {
     return (
-        <Menu inverted fixed= 'top'>
+        <Menu inverted fixed='top'>
             <Container>
                 <Menu.Item header>
-                    <img src="/assets/logo.png" alt="logo" />
+                    <img src="/assets/logo.png" alt="logo" style={{marginRight: '10px'}}/>
                     Reactivities
                 </Menu.Item>
                 <Menu.Item name='Activities' />
