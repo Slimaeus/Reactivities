2ad8756c00ca199f83752d0d194ca15cc08ffb82
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-01-29 10:48:51 +0700

    Added MediatR

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Persistence;
+using MediatR;
+using Application.Activities;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +22,7 @@ builder.Services.AddCors(opt =>
         policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
     });
 });
+builder.Services.AddMediatR(typeof(List.Handler));
 
 var app = builder.Build();
 
