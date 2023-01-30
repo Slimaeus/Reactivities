db63f8de82583f6da541f70cb3dda5f4f1dc28f4
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-01-30 16:01:41 +0700

    Mobx strict mode

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -14,16 +14,23 @@ export default class ActivityStore {
     }
 
     loadingActivies = async () => {
-        this.loadingInitial = true
+        this.setLoadingInitial(true)
         try {
             const activities = await agent.Activities.list()
             activities.forEach(activity => {
                 activity.date = activity.date.split('T')[0]
                 this.activities.push(activity)
+                this.setLoadingInitial(false)
+
             })
-            this.loadingInitial = false;
         } catch (error) {
             console.log(error)
+            this.setLoadingInitial(false)
+
         }
+    }
+
+    setLoadingInitial = (state: boolean) => {
+        this.loadingInitial = state
     }
 }
