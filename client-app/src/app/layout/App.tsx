94fdac80544560c60e619f6582d9c3d5b67e586e
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-01-30 22:17:08 +0700

    Use Route Params

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,10 +1,6 @@
-import { useEffect } from 'react';
 import { Container } from 'semantic-ui-react';
 
 import NavBar from './NavBar';
-import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import { LoadingComponent } from './LoadingComponent';
-import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
 import { Outlet } from 'react-router-dom';
 
