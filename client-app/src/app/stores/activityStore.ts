694a49180c4d5c4fb5c60122d448c3942729d3db
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-01-30 15:48:23 +0700

    Mobx Action

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,12 +1,13 @@
-import { observable } from "mobx"
-import { makeObservable } from "mobx"
+import { makeAutoObservable } from "mobx"
 
 export default class ActivityStore {
     title = 'Hello from Mobx'
 
     constructor() {
-        makeObservable(this, {
-            title: observable
-        })
+        makeAutoObservable(this)
+    }
+
+    setTitle = () => {
+        this.title = this.title + '!'
     }
 }
