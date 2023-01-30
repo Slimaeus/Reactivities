790553508d08c5eafa79e4479a095d3b14558bdb
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-01-30 21:28:37 +0700

    Add Details links

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,8 +1,6 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'semantic-ui-css/semantic.min.css'
 import './app/layout/style.css';
-import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import { StoreContext, store } from './app/stores/store';
 import { RouterProvider } from 'react-router-dom';
